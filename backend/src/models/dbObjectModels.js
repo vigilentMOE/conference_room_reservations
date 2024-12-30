4b4a3cb8633f15ce6a9a65fab30fbc9ae6b87869
vigmoe <rockred12@outlook.com>
2024-12-30 03:24:15 -0500

    feat(gitops): added basic nginx setup, changed docker compose setup for postgres and pgadmin

--- backend/src/models/dbObjectModels.js
+++ backend/src/models/dbObjectModels.js
@@ -1,6 +1,17 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
+/**
+ * Represents a Conference Room in the reservation system.
+ *
+ * @typedef {Object} ConferenceRoom
+ * @property {number} id - The unique identifier for the conference room.
+ * @property {string} name - The name of the conference room.
+ * @property {number} capacity - The capacity of the room, must be at least 1.
+ * @property {string[]} features - An array of features available in the room.
+ * @property {string} location - The physical location of the conference room.
+ * @property {boolean} isAvailable - Indicates if the room is currently available.
+ */
 export const ConferenceRoom = sequelize.define("ConferenceRoom", {
   id: {
     type: DataTypes.INTEGER,
@@ -32,6 +43,15 @@ export const ConferenceRoom = sequelize.define("ConferenceRoom", {
   },
 });
 
+/**
+ * Represents a reservation in the conference room reservation system.
+ *
+ * @typedef {Object} Reservation
+ * @property {number} id - The unique identifier for the reservation.
+ * @property {Date} startTime - The start time of the reservation.
+ * @property {Date} endTime - The end time of the reservation.
+ * @property {number} userId - The ID of the user who made the reservation.
+ */
 export const Reservation = sequelize.define("Reservation", {
   id: {
     type: DataTypes.INTEGER,
