253325e16f1792d95a7515c0df422b97788a3f10
vigmoe <rockred12@outlook.com>
2025-01-06 20:39:52 -0500

    chore(dev): babel setup for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject: `import { h, Fragment } from 'preact'`
+  }
 })
