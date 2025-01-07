fa7ad01bdaaf077a176b8a2b73b5dc3bae191c63
vigmoe <rockred12@outlook.com>
2025-01-07 14:05:14 -0500

    feat(dev): git webhook implementation

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -3,6 +3,13 @@ import preactLogo from "./assets/preact.svg";
 import viteLogo from "/vite.svg";
 import "./app.css";
 
+/**
+ * Main application component that demonstrates basic Vite and Preact functionality.
+ * Includes a counter implementation and links to documentation
+ * 
+ * @component
+ * @returns {JSX.Element} A fragment 
+ */
 export function App() {
   const [count, setCount] = useState(0);
 
@@ -10,14 +17,14 @@ export function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+          <img src={preactLogo} className="logo preact" alt="Preact logo" />
         </a>
       </div>
       <h1>Vite + Preact</h1>
-      <div class="card">
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -30,12 +37,13 @@ export function App() {
         <a
           href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
           target="_blank"
+          rel="noreferrer"
         >
           create-preact
         </a>
         , the official Preact + Vite starter
       </p>
-      <p class="read-the-docs">
+      <p className="read-the-docs">
         Click on the Vite and Preact logos to learn more
       </p>
     </>
